00e416cff3ba2ea2d627adeb010717073958d179
whibaek <jibaek0513@gmail.com>
2022-09-29 02:20:40 +0900

    add photo upload

--- src/main/java/hello/upload/controller/SpringUploadController.java
+++ src/main/java/hello/upload/controller/SpringUploadController.java
@@ -1,15 +1,16 @@
 package hello.upload.controller;
+import hello.upload.domain.PBClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.io.IOException;
+
 @Slf4j
 @Controller
 @RequestMapping("/spring")
@@ -20,13 +21,20 @@ public class SpringUploadController {
     public String newFile() {
         return "upload-form";
     }
+
     @PostMapping("/upload")
-    public String saveFile(@RequestParam MultipartFile file) throws IOException {
+    public String saveFile(@RequestParam MultipartFile file, Model model) throws Exception {
         if (!file.isEmpty()) {
             String fullPath = fileDir + "testFile.jpg";
             log.info("파일 저장 fullPath={}", fullPath);
             file.transferTo(new File(fullPath));
         }
+
+        // Thread.sleep(10000); // 사진 저장되는데 걸리는 시간
+
+        model.addAttribute("str", new PBClass().ImplementPython());
+
+
         return "testForm";
     }
 }
